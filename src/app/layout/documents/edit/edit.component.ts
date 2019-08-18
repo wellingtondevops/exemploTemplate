789a795cc361449b7c81e7043c902e62ee768e14
Vanessa Souto <vanessasoutoc@gmail.com>
2019-08-18 16:55:26 -0300

    edit document

--- src/app/layout/documents/edit/edit.component.ts
+++ src/app/layout/documents/edit/edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DocumentsService } from 'src/app/services/documents/documents.service';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { SuccessMessagesService } from 'src/app/utils/success-messages.service';
+import { ErrorMessagesService } from 'src/app/utils/error-messages.service';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
+  id: string;
+  document: Document
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private documentsSrv: DocumentsService,
+    private fb: FormBuilder,
+    private successMsgSrv: SuccessMessagesService,
+    private errorMsg: ErrorMessagesService
+  ) {
+
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.getDocument();
+   }
 
   ngOnInit() {
+  }
+
+  getDocument() {
+    this.documentsSrv.document(this.id).subscribe(data => {
+      console.log(data)
+    })
   }
 
 }
