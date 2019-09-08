e052393b8887b44b19e808d9257b9cb98790e4c8
Vanessa Souto <vanessasoutoc@gmail.com>
2019-09-08 17:12:10 -0300

    formulario de localizacao

--- src/app/layout/register-file/list/list.component.ts
+++ src/app/layout/register-file/list/list.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { routerTransition } from 'src/app/router.animations';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { SuccessMessagesService } from 'src/app/utils/success-messages.service';
 import { ErrorMessagesService } from 'src/app/utils/error-messages.service';
 import { Router } from '@angular/router';
+import { CompaniesService } from 'src/app/services/companies/companies.service';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import _ from 'lodash';
+import { StorehousesService } from 'src/app/services/storehouses/storehouses.service';
 
 @Component({
   selector: 'app-list',
@@ -12,20 +17,99 @@ import { Router } from '@angular/router';
   animations: [routerTransition()]
 })
 export class ListComponent implements OnInit {
+  companies: any = [];
   registerFileForm: FormGroup;
+  storeHouses: any = [];
+  inputBlock: Boolean = false;
 
   constructor(
     private fb: FormBuilder,
     private successMsgSrv: SuccessMessagesService,
     private errorMsg: ErrorMessagesService,
-    private _route: Router
+    private _route: Router,
+    private companiesSrv: CompaniesService,
+    private storeHousesSrv: StorehousesService,
   ) { }
 
   ngOnInit() {
     this.registerFileForm = this.fb.group({
       company: this.fb.control('', [Validators.required]),
+      storehouse: this.fb.control('', [Validators.required]),
       location: this.fb.control('')
     });
+
+    this.getCompanies();
+    this.getStoreHouses();
   }
 
+  get company() {
+    return this.registerFileForm.get('company');
+  }
+  get storehouse() {
+    return this.registerFileForm.get('storehouse');
+  }
+
+  blockInputs() {
+    !this.inputBlock ? (this.inputBlock = true) : (this.inputBlock = false);
+  }
+
+  formatter = (x: { name: string }) => x.name;
+
+  getCompanies() {
+    this.companiesSrv.searchCompanies().subscribe(
+      data => {
+        this.companies = data.items;
+      },
+      error => {
+        this.errorMsg.errorMessages(error);
+        console.log('ERROR: ', error);
+      }
+    );
+  }
+
+  getStoreHouses() {
+    this.storeHousesSrv.searchStorehouses().subscribe(
+      data => {
+        // this.loading = false;
+        this.storeHouses = data.items;
+      },
+      error => {
+        // this.loading = false;
+        this.errorMsg.errorMessages(error);
+        console.log('ERROR: ', error);
+      }
+    );
+  }
+
+  search = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(storehouse =>
+        storehouse.length < 2
+          ? []
+          : _.filter(this.storeHouses, v => v.name.toLowerCase().indexOf(storehouse.toLowerCase()) > -1).slice(0, 10)
+      )
+    );
+
+  searchCompany = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(company => {
+        var res;
+        if (company.length < 2) [];
+        else var res = _.filter(this.companies, v => v.name.toLowerCase().indexOf(company.toLowerCase()) > -1).slice(0, 10);
+        return res;
+      })
+    );
+}
+@Pipe({
+  name: 'enumToArray'
+})
+export class EnumToArrayPipe implements PipeTransform {
+  transform(data: Object) {
+    const keys = Object.keys(data);
+    return keys.slice(keys.length / 2);
+  }
 }
