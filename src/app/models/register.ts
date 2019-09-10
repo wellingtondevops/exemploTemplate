bb316751e0a5d76357549718a48767673bea50b5
vanessa <vanessasoutoc@gmail.com>
2019-09-09 23:41:07 -0300

    model register, service volume and register

---
+++ src/app/models/register.ts
@@ -0,0 +1,29 @@
+export class Register {
+    _links: {
+      self: string
+    };
+    _id: string;
+    doct: {
+        _id: string;
+        label: [Label];
+        tag: []
+    }
+  }
+  
+  export class RegistersList {
+    _links: {
+      self: string;
+      totalPage: number;
+      currentPage: number;
+      foundItems: number;
+      next: string;
+    };
+    items: Register[];
+  }
+  
+  export class Label {
+      namefield: string;
+      typeField: string;
+      _id: string;
+      uniq: boolean
+  }
