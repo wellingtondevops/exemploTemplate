095672b360e13a487cdc08068157a946cffba7ba
vanessa <vanessasoutoc@gmail.com>
2019-09-30 20:56:25 -0300

    btn back all shows

--- src/app/layout/documents/documents.module.ts
+++ src/app/layout/documents/documents.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { DocumentsRoutingModule } from './documents-routing.module';
-import { PageHeaderModule, DatatablesModule, ButtonsCustomModule } from 'src/app/shared';
+import { PageHeaderModule, DatatablesModule, ButtonsCustomModule, ButtonBackModule } from 'src/app/shared';
 import { NewComponent, EnumToArrayPipe } from './new/new.component';
 import { EditComponent } from './edit/edit.component';
 import { ShowComponent } from './show/show.component';
@@ -21,6 +21,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
         DatatablesModule,
         TranslateModule,
         ButtonsCustomModule,
+        ButtonBackModule,
         NgxLoadingModule.forRoot({})
     ]
 })
