57cd1ba4733a37e4f5e88609fba25a7ede2a5a87
vanessa <vanessasoutoc@gmail.com>
2019-10-13 15:16:45 -0300

    display form show

--- src/app/layout/archives/show/show.component.ts
+++ src/app/layout/archives/show/show.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ArquivesService } from 'src/app/services/archives/archives.service';
+import { Archive } from 'src/app/models/archive';
 
 @Component({
   selector: 'app-show',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show.component.scss']
 })
 export class ShowComponent implements OnInit {
+  id: string;
+  archive: Archive;
+  loading: Boolean = false;
 
-  constructor() { }
+  constructor(
+    private _route: Router,
+    private route: ActivatedRoute,
+    private archiveSrv: ArquivesService
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.getArquive();
   }
 
+  getArquive(){
+    this.archiveSrv.archive(this.id).subscribe(data => {
+      console.log(data)
+      this.archive = data;
+    }, error => {
+      console.log('ERROR: ', error)
+    })
+  }
+
+  mapLabel(labels, tags) {
+    var obj = ''
+    labels.map((item, i) => {
+      if (i === (labels.length - 1)) {
+        obj += `${item.namefield}: ${tags[i]}`
+      } else {
+        obj += `${item.namefield}: ${tags[i]} | `
+      }
+    })
+    return obj
+  }
 }
--- src/app/shared/modules/form-index/form-index.component.ts
+++ src/app/shared/modules/form-index/form-index.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, EventEmitter, Injectable, SimpleChanges, Output } fro
 import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig } from '@ngx-formly/core';
 import { Document } from '../../../models/document'
+import { Archive } from 'src/app/models/archive';
+import { isArray } from 'util';
 
 @Component({
     selector: 'app-form-index',
@@ -11,9 +13,11 @@ import { Document } from '../../../models/document'
 @Injectable()
 export class FormIndexComponent {
     @Input() data: Document;
+    @Input() isArchive: Boolean;
     @Output() sendArchive = new EventEmitter();
     firstMoment = false;
     document: Document;
+    archive: Archive;
     form = new FormGroup({});
     model: any;
     fields: any;
@@ -27,10 +31,18 @@ export class FormIndexComponent {
         }
         // tslint:disable-next-line:forin
         for (const propName in changes) {
-            const change = changes[propName];
-            this.document = change.currentValue;
-            this.model = this.objectModel(this.document.label);
-            this.fields = this.arrayLabelForm(this.document.label);
+            if (!this.isArchive) {
+                const change = changes[propName];
+                this.document = change.currentValue;
+                this.model = this.objectModel(this.document.label, null);
+                this.fields = this.arrayLabelForm(this.document.label, null);
+            } else {
+                console.log('else')
+                const change = changes[propName];
+                this.archive = change.currentValue;
+                this.model = this.objectModel(this.archive.doct.label, this.archive.tag);
+                this.fields = this.arrayLabelForm(this.archive.doct.label, this.archive.tag);
+            }
         }
     }
 
@@ -47,16 +59,19 @@ export class FormIndexComponent {
         return type
     }
 
-    arrayLabelForm(labels) {
+    arrayLabelForm(labels, tag) {
         var array = labels.map((element, i) => {
+            var value = tag ? tag[i] : '';
             var result = {
                 id: `formindex${i}`,
                 key: element.namefield,
                 type: this.typeInput(element.typeField),
+                defaultValue: value,
                 templateOptions: {
                     label: element.namefield,
                     placeholder: element.namefield,
                     required: false,
+                    disabled: value ? true : false,
                     keydown: (field, e) => {
                         if (e.which === 13) {
                             var number = i + 1
@@ -69,15 +84,16 @@ export class FormIndexComponent {
             if (i == 0) {
                 result['focus'] = true;
             }
+            console.log('result', result)
             return result
         });
         return array
     }
 
-    objectModel(labels) {
+    objectModel(labels, tag) {
         var newObj = {}
-        labels.map(element => {
-            newObj[element.namefield] = ''
+        labels.map((element, i) => {
+            newObj[element.namefield] = tag ? tag[i] : ''
         })
         return newObj
     }
