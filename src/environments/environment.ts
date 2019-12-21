c311753820986e26fc37b429c056eb1ed55bed47
vanessa <vanessasoutoc@gmail.com>
2019-12-21 15:51:42 -0300

    url local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'https://www.archio-api.online',
-  apiUrlUpload: 'https://archioqa.appspot.com',
+  apiUrl: 'http://localhost:3000',
+  apiUrlUpload: 'http://localhost:2000',
   production: false
 };
 
