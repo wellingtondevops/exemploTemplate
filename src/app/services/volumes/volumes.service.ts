2bddac6846cb87c3c1b9eafb1df96d4f1c11971f
Vanessa Silva Souto da Conceição <VenessaConceicao@PIMM-C02Z6A8YLVDC.local>
2020-03-27 14:36:29 -0300

    correção em registrar arquivo

--- src/app/services/volumes/volumes.service.ts
+++ src/app/services/volumes/volumes.service.ts
@@ -17,63 +17,70 @@ export class VolumesService {
   searchVolumes(formData, page) {
     if (page) {
       return this.http.post<VolumeList>(`${url}/volumes/search?_page=${page.pageNumber}&size=10`, formData)
-      .pipe(
+        .pipe(
           tap(data => data)
-      );
+        );
     } else {
       return this.http.post<VolumeList>(`${url}/volumes/search?size=10`, formData)
-      .pipe(
+        .pipe(
           tap(data => data)
-      );
+        );
     }
   }
 
   volumes(page) {
     if (page) {
       return this.http.get<VolumeList>(`${url}/volumes?_page=${page.pageNumber}&size=10`)
-      .pipe(
+        .pipe(
           tap(data => data)
-      );
+        );
     } else {
       return this.http.get<VolumeList>(`${url}/volumes?size=10`)
-      .pipe(
+        .pipe(
           tap(data => data)
-      );
+        );
     }
   }
 
   deleteVolume(volume) {
     return this.http.delete<Volume>(`${url}/volumes/${volume}`)
-    .pipe(
-      tap(data => data)
-    );
+      .pipe(
+        tap(data => data)
+      );
   }
 
   volume(id) {
     return this.http.get<Volume>(`${url}/volumes/${id}`)
-    .pipe(
+      .pipe(
         tap(data => data)
-    );
+      );
   }
 
   updateVolume(volume) {
     return this.http.put<Volume>(`${url}/volumes/${volume._id}`, volume)
-    .pipe(
-      tap(data => data)
-    );
+      .pipe(
+        tap(data => data)
+      );
   }
 
   newVolume(volume) {
     return this.http.post<Volume>(`${url}/volumes`, volume)
-    .pipe(
-      tap(data => data)
-    );
+      .pipe(
+        tap(data => data)
+      );
   }
 
-  listvolume(storehouse_id, company_id, location, departament){
-    return this.http.get<VolumeList>(`${url}/listvolumes?storehouse=${storehouse_id}&company=${company_id}&location=${location}&departament=${departament}&size=10`)
-    .pipe(
-      tap(data => data)
-    )
+  listvolume(storehouse_id, company_id, location, departament, page = null) {
+    if (page) {
+      return this.http.get<VolumeList>(`${url}/listvolumes?storehouse=${storehouse_id}&company=${company_id}&location=${location}&departament=${departament}&_page=${page.pageNumber}&size=10`)
+        .pipe(
+          tap(data => data)
+        );
+    } else {
+      return this.http.get<VolumeList>(`${url}/listvolumes?storehouse=${storehouse_id}&company=${company_id}&location=${location}&departament=${departament}&size=10`)
+        .pipe(
+          tap(data => data)
+        )
+    }
   }
 }
