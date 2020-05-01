541bde43a848b7967195042c4cc05a8ac8788417
Vanessa Silva Souto da Conceição <VenessaConceicao@PIMM-C02Z6A8YLVDC.local>
2020-05-01 18:34:52 -0300

    ng test and lint

--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -1,26 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('LoginService', () => {
-  beforeEach(() => TestBed.configureTestingModule(
-    {
-      declarations: [],
-      imports: [HttpClientTestingModule],
-      providers: []
-    }
-  ));
+  let injector: TestBed;
+  let service: AuthService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule(
+      {
+        declarations: [],
+        imports: [HttpClientTestingModule],
+        providers: [AuthService]
+      }
+    )
+
+    injector = getTestBed();
+    service = injector.get(AuthService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });  
 
   it('should be login user', () => {
     const service: AuthService = TestBed.get(AuthService);
     let logged = false;
-    service.login({email: 'gedearchive@gmail.com', password: '1c922d'}).subscribe(res => {
+    service.login({ email: 'gedearchive@gmail.com', password: '1c922d' }).subscribe(res => {
       if (res.accessToken) {
         logged = true;
       }
     }, error => {
 
     });
+    console.log(logged)
     expect(logged).toBeTruthy();
   });
 });
