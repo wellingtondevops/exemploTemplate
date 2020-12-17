868f6da4ff71ad69ab8b6361ad445bf956486ff1
Wellington  Carvalho de Oliveira <wellingtondevops@gmail.com>
2020-12-17 11:13:48 -0300

    alteração no componente de exibir pdf melhoria na funcao case-insensitive

--- src/app/utils/case-insensitive.ts
+++ src/app/utils/case-insensitive.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@angular/core';
-import createNumberMask from 'text-mask-addons/dist/createNumberMask'
+import createNumberMask from 'text-mask-addons/dist/createNumberMask';
 import { CompaniesRoutingModule } from '../layout/companies/companies-routing.module';
 
 @Injectable()
 export class CaseInsensitive {
-  constructor(){
+  constructor() {
 
   }
 
   replaceSpecialChars = function(str) {
-    str = str.replace(/[ÀÁÂÃÄÅ]/, "A");
-    str = str.replace(/[àáâãäå]/, "a");
-    str = str.replace(/[íìî]/, "i")
-    str = str.replace(/[ÌÍÎ]/, "I")
-    str = str.replace(/[éèê]/, "e");
-    str = str.replace(/[óòôõ]/, "o");
-    str = str.replace(/[ÓÒÕÔ]/, "O");
-    str = str.replace(/[ÈÉÊË]/, "E");
-    str = str.replace(/[Ç]/, "C");
-    str = str.replace(/[ç]/, "c");
+    str = str.replace(/[ÀÁÂÃÄÅ]/, 'A');
+    str = str.replace(/[àáâãäå]/, 'a');
+    str = str.replace(/[íìî]/, 'i');
+    str = str.replace(/[ÌÍÎ]/, 'I');
+    str = str.replace(/[éèê]/, 'e');
+    str = str.replace(/[óòôõ]/, 'o');
+    str = str.replace(/[ÓÒÕÔ]/, 'O');
+    str = str.replace(/[ÈÉÊË]/, 'E');
+    str = str.replace(/[Ç]/, 'C');
+    str = str.replace(/[ç]/, 'c');
     // o resto
 
-    return str.replace(/[^a-z0-9]/gi, '');
-  }
+    return str;
+  };
 }
