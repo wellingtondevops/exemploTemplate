b4e6196d39e23f7881006787dbaf95ac953d6a57
Wellington  Carvalho de Oliveira <wellingtondevops@gmail.com>
2020-12-17 11:44:40 -0300

    inserido a regra do U no case-insensitive

--- src/app/utils/case-insensitive.ts
+++ src/app/utils/case-insensitive.ts
@@ -11,11 +11,14 @@ export class CaseInsensitive {
   replaceSpecialChars = function(str) {
     str = str.replace(/[ÀÁÂÃÄÅ]/, 'A');
     str = str.replace(/[àáâãäå]/, 'a');
+
     str = str.replace(/[íìî]/, 'i');
     str = str.replace(/[ÌÍÎ]/, 'I');
     str = str.replace(/[éèê]/, 'e');
     str = str.replace(/[óòôõ]/, 'o');
     str = str.replace(/[ÓÒÕÔ]/, 'O');
+    str = str.replace(/[ÚÙÛÜ]/, 'U');
+    str = str.replace(/[ùúûü]/, 'u');
     str = str.replace(/[ÈÉÊË]/, 'E');
     str = str.replace(/[Ç]/, 'C');
     str = str.replace(/[ç]/, 'c');
