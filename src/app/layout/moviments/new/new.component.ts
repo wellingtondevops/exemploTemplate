19ce8c373c08c3d18d8a68ff74a67920741e1e53
Vanessa Silva Souto da Conceição <VenessaConceicao@ip-192-168-100-3.sa-east-1.compute.internal>
2021-01-07 08:20:27 -0300

    moviment new ok

--- src/app/layout/moviments/new/new.component.ts
+++ src/app/layout/moviments/new/new.component.ts
@@ -15,6 +15,8 @@ import { NgbdModalConfirmComponent } from 'src/app/shared/modules/ngbd-modal-con
 import { ErrorMessagesService } from 'src/app/utils/error-messages/error-messages.service';
 import { SuccessMessagesService } from 'src/app/utils/success-messages/success-messages.service';
 import _ from 'lodash';
+import { RequestersService } from 'src/app/services/requesters/requesters.service';
+import { Requester, RequesterSearchList } from 'src/app/models/requester';
 
 const MODALS = {
   focusFirst: NgbdModalConfirmComponent
@@ -36,6 +38,8 @@ export class NewComponent implements OnInit {
   public loading: Boolean = false;
   permissionEdit = false;
   permissionDelete = false;
+  requesters: any;
+  requester: any;
   // isUsers = false;
 
   constructor(
@@ -49,38 +53,69 @@ export class NewComponent implements OnInit {
     private fb: FormBuilder,
     private modalService: NgbModal,
     public modal: NgbActiveModal,
-    private _route: Router
+    private _route: Router,
+    private requesterSrv: RequestersService
   ) {
     this.movimentForm = this.fb.group({
       company: this.fb.control(null, [Validators.required]),
-      requester: this.fb.control(null),
-      author: this.fb.control(null),
-      demandDate: this.fb.control(null),
-      itens: this.fb.control(null),
-      processed: this.fb.control(null),
-      title: this.fb.control(null),
-      demand: this.fb.control(null),
-      deadline: this.fb.control(null),
-      move: this.fb.control(null),
-      deliveryFormat: this.fb.control(null),
-      moveNature: this.fb.control(null),
-      nr: this.fb.control(null)
+      requester: this.fb.control(null, [Validators.required]),
+      deadline: this.fb.control(null, [Validators.required]),
+      move: this.fb.control(null, [Validators.required]),
+      deliveryFormat: this.fb.control(null, [Validators.required]),
+      moveNature: this.fb.control(null, [Validators.required]),
     });
   }
 
   ngOnInit() {
     this.getCompanies();
   }
+  
   get companyIpt() {
     return this.movimentForm.get('company');
   }
 
+  get requesterIpt() {
+    return this.movimentForm.get('requester');
+  }
+
+  get deadlineIpt() {
+    return this.movimentForm.get('deadline');
+  }
+
+  get deliveryFormatIpt() {
+    return this.movimentForm.get('deliveryFormat');
+  }
+
+  get moveIpt() {
+    return this.movimentForm.get('move');
+  }
+
+  get moveNatureIpt() {
+    return this.movimentForm.get('moveNature');
+  }
+
   newMoviment() {
-    
-    this.movimentsSrv.newMoviment(this.movimentForm.value).subscribe(
+    let newMoviment = {
+      company: this.movimentForm.value.company._id,
+      requester: this.movimentForm.value.requester._id,
+      loan: this.movimentForm.value.moveNature === 'loan' ? true : false,
+      low: this.movimentForm.value.moveNature === 'low' ? true : false,
+      withdraw: this.movimentForm.value.delivery === 'withdraw' ? true : false,
+      delivery: this.movimentForm.value.delivery === 'delivery' ? true : false,
+      digital: this.movimentForm.value.delivery === 'digital' ? true : false,
+      moveArchive: this.movimentForm.value.move === 'moveArchive' ? true : false,
+      moveVolume: this.movimentForm.value.move === 'moveVolume' ? true : false,
+      emergency: this.movimentForm.value.deadline === 'emergency' ? true : false,
+      normal: this.movimentForm.value.deadline === 'normal' ? true : false
+    }
+    newMoviment = _.omitBy(newMoviment, _.isNil);
+
+    this.movimentsSrv.newMoviment(newMoviment).subscribe(
       data => {
         this.loading = false;
         this.moviment = data;
+        this.successMsgSrv.successMessages('Movimentação cadastrado com sucesso.');
+        this._route.navigate(['/moviments'])
       }, error => {
         this.loading = false;
         this.errorMsg.errorMessages(error);
@@ -89,8 +124,10 @@ export class NewComponent implements OnInit {
     )
   }
 
+  
+
   getCompanies() {
-    this.companiesSrv.companies(null).subscribe(
+    this.companiesSrv.searchCompanies().subscribe(
       data => {
         this.companies = data.items;
       },
@@ -99,6 +136,23 @@ export class NewComponent implements OnInit {
         console.log('ERROR: ', error);
       }
     );
+  }
+
+  getRequesters(company) {
+    this.requesterSrv.listRequesterByCompany(company).subscribe(
+      data => {
+        this.requesters = data.items;
+      },
+      error => {
+        this.errorMsg.errorMessages(error);
+        console.log('ERROR: ', error);
+      }
+    );
+  }
+
+  getCompany(company) {
+    console.log(company)
+    this.getRequesters(company.item._id);
   }
 
   formatter = (x: { name: string }) => x.name;
@@ -113,6 +167,21 @@ export class NewComponent implements OnInit {
           [];
         } else {
           res = _.filter(this.companies, v => v.name.toLowerCase().indexOf(company.toLowerCase()) > -1).slice(0, 10);
+        }
+        return res;
+      })
+    )
+
+    searchRequester = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(requester => {
+        let res;
+        if (requester.length < 2) {
+          [];
+        } else {
+          res = _.filter(this.requesters, v => v.name.toLowerCase().indexOf(requester.toLowerCase()) > -1).slice(0, 10);
         }
         return res;
       })
