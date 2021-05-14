bd871a06464a112d8159190d19375225feb6d694
Vanessa Silva Souto da Conceição <VenessaConceicao@ip-192-168-100-7.sa-east-1.compute.internal>
2021-05-14 19:14:00 -0300

    select all bug fix

--- src/app/layout/archives/list/list.component.ts
+++ src/app/layout/archives/list/list.component.ts
@@ -327,7 +327,7 @@ export class ListComponent implements OnInit {
   getDocuments(company_id) {
     this.documentsSrv.searchDocuments(company_id).subscribe(
       data => {
-        this.documents.push({_id:0,name: 'Selecionar todos'}, ...data.items)
+        this.documents = data.items;
       },
       error => {
         this.errorMsg.errorMessages(error);
