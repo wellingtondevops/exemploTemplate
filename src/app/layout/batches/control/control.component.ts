0677bbd453062ba9e03c63da781c47ac0249928a
Vanessa Silva Souto da Conceição <VenessaConceicao@PIMM-C02Z6A8YLVDC.local>
2021-09-26 19:57:53 -0300

    re-upload

--- src/app/layout/batches/control/control.component.ts
+++ src/app/layout/batches/control/control.component.ts
@@ -17,165 +17,188 @@ import { DomSanitizer } from '@angular/platform-browser'
 
 
 @Component({
-  selector: 'app-control',
-  templateUrl: './control.component.html',
-  styleUrls: ['./control.component.scss'],
-  animations: [routerTransition()]
+    selector: 'app-control',
+    templateUrl: './control.component.html',
+    styleUrls: ['./control.component.scss'],
+    animations: [routerTransition()]
 })
 export class ControlComponent implements OnInit {
-  progressInfos: any = [];
-  fileInfos: any = [];
-  batch: Batch;
-  batchImages: any;
-  id: string;
-  loading: Boolean = true;
-  page = new Page();
-  // pageImages = new Page();
-  myFiles: any = [];
-  uploadResponse: any = { status: 'progress', message: 0 };
-  message: string;
+    progressInfos: any = [];
+    fileInfos: any = [];
+    batch: Batch;
+    batchImages: any;
+    id: string;
+    loading: Boolean = true;
+    page = new Page();
+    // pageImages = new Page();
+    myFilesInputSelect: any = [];
+    myFiles: any = [];
+    uploadResponse: any = { status: 'progress', message: 0 };
+    message: string;
 
-  myForm = new FormGroup({
-    file: new FormControl('', [Validators.required])
-  });
+    myForm = new FormGroup({
+        file: new FormControl('', [Validators.required])
+    });
 
-  constructor(
-    private _route: Router,
-    private route: ActivatedRoute,
-    private fb: FormBuilder,
-    private successMsgSrv: SuccessMessagesService,
-    private errorMsg: ErrorMessagesService,
-    private batchesSrv: BatchesService,
-    private filesSrv: FilesService,
-    private pipes: Pipes,
-    private domSanitizer: DomSanitizer
-  ) { }
+    constructor(
+        private _route: Router,
+        private route: ActivatedRoute,
+        private fb: FormBuilder,
+        private successMsgSrv: SuccessMessagesService,
+        private errorMsg: ErrorMessagesService,
+        private batchesSrv: BatchesService,
+        private filesSrv: FilesService,
+        private pipes: Pipes,
+        private domSanitizer: DomSanitizer
+    ) { }
 
-  ngOnInit() {
-    $(document).ready(function () {
-      // var $modal = $('#right-bottom');
-      $('.cus-modal').click(function () {
+    ngOnInit() {
+        /* $(document).ready(function () {
+            // var $modal = $('#right-bottom');
+            $('.cus-modal').click(function () {
+                $('body').removeClass('cus-modal-open');
+                $('.in').removeClass('in');
+            });
+        }); */
+        this.id = this.route.snapshot.paramMap.get('id');
+        this.getBatch();
+    }
+
+    openModal() {
+        $('body').addClass('cus-modal-open');
+        const target = '#right-bottom';
+        $(target).addClass('in');
+    }
+
+    close(){
         $('body').removeClass('cus-modal-open');
-        $('.in').removeClass('in');
-      });
-    });
-    this.id = this.route.snapshot.paramMap.get('id');
-    this.getBatch();
-  }
-
-  openModal() {
-    $('body').addClass('cus-modal-open');
-    const target = '#right-bottom';
-    $(target).addClass('in');
-  }
-
-  selectedImg(e) {
-    console.log(e.target.id)
-  }
-
-  isPdf(img) {
-    console.log(1)
-    return this.pipes.isPdf(img.url)
-  }
-
-  sanitizeUrl(url) {
-    return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
-  }
-
-  onFileChange(event) {
-    for (var i = 0; i < event.target.files.length; i++) {
-      // files
-      this.myFiles.push(event.target.files[i]);
-    }
-  }
-
-  getBatch() {
-    this.loading = true;
-    this.batchesSrv.batch(this.id).subscribe(data => {
-      this.loading = false;
-      this.batch = data;
-      this.getBatchImages(1, 50);
-    }, error => {
-      console.log('ERROR: ', error);
-      this.loading = false;
-      this.errorMsg.errorMessages(error);
-    })
-  }
-
-  getBatchImages(pageInfo = null, size = 50) {
-    this.loading = true;
-    if (pageInfo) {
-      this.page.pageNumber = pageInfo;
+        $('.in').removeClass('in'); 
+        this.clearUpload()
     }
 
-    this.batchesSrv.batchImages(this.id, this.page, size).subscribe(data => {
-      this.loading = false;
-      this.batchImages = data.items;
-      this.page.totalElements = data._links.foundItems;
-      this.page.size = data._links.totalPage;
-    }, error => {
-      this.loading = false;
-      this.errorMsg.errorMessages(error)
-    })
-  }
-
-  clearUpload() {
-    // this.progressInfos = [];
-    this.myForm.patchValue({
-      file: '',
-    })
-    this.myFiles = [];
-  }
-
-  submit() {
-    this.progressInfos = [];
-    const formData = new FormData();
-    formData.append('document', this.batch.doct._id);
-    formData.append('company', this.batch.company._id);
-    formData.append('batch', this.batch._id);
-    formData.append('ind', 'false');
-    for (var i = 0; i < this.myFiles.length; i++) {
-      this.progressInfos.push({ value: 0, fileName: this.myFiles[i].name, class: 'progress-bar progress-bar-info progress-bar-striped' });
-      this.openModal();
-      formData.delete("files");
-      formData.append("files", this.myFiles[i]);
-      this.upload(formData, i)
-      if(i === (this.myFiles.length - 1)){
-        this.clearUpload();
-      }
+    selectedImg(e) {
+        console.log(e.target.id)
     }
-  }
 
-  upload(formData, i) {
-    this.filesSrv.filesProgress(formData).subscribe(event => {
-      if (event.type === HttpEventType.UploadProgress) {
-        this.progressInfos[i].value = Math.round(100 * event.loaded / event.total);
-      } else if (event instanceof HttpResponse) {
-        this.fileInfos[i] = event.body[0];
-        this.progressInfos[i].class = 'progress-bar bg-success progress-bar-striped';
-        this.batchImages.push(event.body[0]);
-        this.page.totalElements++;
-      }
-    }, error => {
-      this.progressInfos[i].class = 'progress-bar bg-danger progress-bar-striped';
-    });
-  }
+    isPdf(img) {
+        console.log(1)
+        return this.pipes.isPdf(img.url)
+    }
 
-  deleteBatch() {
-    this.loading = true;
-    this.batchesSrv.delete(this.id).subscribe(data => {
-      this.loading = false;
-      this.successMsgSrv.successMessages('Lote excluído com sucesso.');
+    sanitizeUrl(url) {
+        return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+    }
 
-    }, error => {
-      console.log('ERROR: ', error);
-      this.loading = false;
-      this.errorMsg.errorMessages(error);
-    })
-  }
+    onFileChange(event) {
+        this.myFilesInputSelect = [];
+        for (var i = 0; i < event.target.files.length; i++) {
+            // files
+            this.myFilesInputSelect.push(event.target.files[i]);
+        }
+    }
 
-  toIndex(){
-      this._route.navigate(['/index', this.id])
-  }
+    getBatch() {
+        this.loading = true;
+        this.batchesSrv.batch(this.id).subscribe(data => {
+            this.loading = false;
+            this.batch = data;
+            this.getBatchImages(1, 50);
+        }, error => {
+            console.log('ERROR: ', error);
+            this.loading = false;
+            this.errorMsg.errorMessages(error);
+        })
+    }
+
+    getBatchImages(pageInfo = null, size = 50) {
+        this.loading = true;
+        if (pageInfo) {
+            this.page.pageNumber = pageInfo;
+        }
+
+        this.batchesSrv.batchImages(this.id, this.page, size).subscribe(data => {
+            this.loading = false;
+            this.batchImages = data.items;
+            this.page.totalElements = data._links.foundItems;
+            this.page.size = data._links.totalPage;
+        }, error => {
+            this.loading = false;
+            this.errorMsg.errorMessages(error)
+        })
+    }
+
+    clearUpload() {
+        this.progressInfos = [];
+        this.myForm.patchValue({
+            file: '',
+        })
+        this.myFiles = [];
+    }
+
+    submit() {
+        
+        const formData = new FormData();
+        formData.append('document', this.batch.doct._id);
+        formData.append('company', this.batch.company._id);
+        formData.append('batch', this.batch._id);
+        formData.append('ind', 'false');
+        for (var i = 0; i < this.myFilesInputSelect.length; i++) {
+            console.log(this.myFiles)
+            this.myFiles.push(this.myFilesInputSelect[i])
+            this.progressInfos.push({ value: 0, fileName: this.myFiles[i].name, file: this.myFiles[i], class: 'progress-bar progress-bar-info progress-bar-striped' });
+            this.openModal();
+            formData.delete("files");
+            formData.append("files", this.myFiles[i]);
+            let last = this.myFiles.length;
+            last -= 1
+            this.upload(formData, last)
+        }
+    }
+
+    upload(formData, i) {
+        this.filesSrv.filesProgress(formData).subscribe(event => {
+            if (event.type === HttpEventType.UploadProgress) {
+                this.progressInfos[i].value = Math.round(100 * event.loaded / event.total);
+            } else if (event instanceof HttpResponse) {
+                this.fileInfos[i] = event.body[0];
+                this.progressInfos[i].class = 'progress-bar bg-success progress-bar-striped';
+                this.batchImages.push(event.body[0]);
+                this.progressInfos[i].error = false;
+                this.page.totalElements++;
+            }
+        }, error => {
+            console.log(error)
+            this.progressInfos[i].error = true;
+            this.progressInfos[i].class = 'progress-bar bg-danger progress-bar-striped';
+        });
+    }
+
+    reUpload(file, position){
+        const formData = new FormData();
+        formData.append('document', this.batch.doct._id);
+        formData.append('company', this.batch.company._id);
+        formData.append('batch', this.batch._id);
+        formData.append('ind', 'false');
+        formData.delete("files");
+        formData.append("files", this.myFiles[position]);
+        this.upload(formData, position)
+    }
+
+    deleteBatch() {
+        this.loading = true;
+        this.batchesSrv.delete(this.id).subscribe(data => {
+            this.loading = false;
+            this.successMsgSrv.successMessages('Lote excluído com sucesso.');
+
+        }, error => {
+            console.log('ERROR: ', error);
+            this.loading = false;
+            this.errorMsg.errorMessages(error);
+        })
+    }
+
+    toIndex() {
+        this._route.navigate(['/index', this.id])
+    }
 
 }
