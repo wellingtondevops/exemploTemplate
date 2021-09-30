c8e6b9d51193caef6d0fb07d62346f18353493fc
wellingtondevops <wellingtondevops@gmail.com>
2021-09-30 11:19:37 -0300

    enviroment cluster

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,17 @@
 export const environment = {
-    apiUrl: 'https://main.archio.com.br',
-    exportUrl: 'https://export.archio.com.br',
-    apiUrlUpload: 'https://archioqa.appspot.com',
-  production: true
+
+  apiUrl: 'https://archiomain.archio.com.br/',
+  apiUrlUpload: 'https://archioqa.appspot.com',
+  exportUrl: 'https://exportarchives.archio.com.br',
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyCy9X4r36l6o1PLn4DuVdFpO41P5SmA57g',
+    authDomain: 'archionotifier.firebaseapp.com',
+    databaseURL: 'https://archionotifier-default-rtdb.firebaseio.com',
+    projectId: 'archionotifier',
+    storageBucket: 'archionotifier.appspot.com',
+    messagingSenderId: '805529833577',
+    appId: '1:805529833577:web:9e5bfaa56d5a4b2ff4d2f8',
+    measurementId: 'G-DWEG5M9FGZ'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,17 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+export const environment = {
 
- export const environment = {
-   apiUrl: 'http://localhost:3000',
-   apiUrlUpload: 'http://localhost:2000',
-   exportUrl: 'http://34.69.67.148:4000',
-   production: false
- };
-/* export const environment = {
-
-  apiUrl: 'http://34.69.104.113:3000/',
-  apiUrlUpload: 'http://localhost:2000',
-  exportUrl: 'http://34.69.67.148:4000/',
-  production: false
-}; */
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  apiUrl: 'https://archiomain.archio.com.br/',
+  apiUrlUpload: 'https://archioqa.appspot.com',
+  exportUrl: 'https://exportarchives.archio.com.br',
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyCy9X4r36l6o1PLn4DuVdFpO41P5SmA57g',
+    authDomain: 'archionotifier.firebaseapp.com',
+    databaseURL: 'https://archionotifier-default-rtdb.firebaseio.com',
+    projectId: 'archionotifier',
+    storageBucket: 'archionotifier.appspot.com',
+    messagingSenderId: '805529833577',
+    appId: '1:805529833577:web:9e5bfaa56d5a4b2ff4d2f8',
+    measurementId: 'G-DWEG5M9FGZ'
+  }
+};
