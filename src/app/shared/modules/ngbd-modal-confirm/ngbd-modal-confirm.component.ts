5d78ed63d5bc681e7ac37cd9d38aee7da007b991
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2021-10-06 17:05:47 -0300

    update melhorias v1
    versão 1.0
    notificações de erros e botões

--- src/app/shared/modules/ngbd-modal-confirm/ngbd-modal-confirm.component.ts
+++ src/app/shared/modules/ngbd-modal-confirm/ngbd-modal-confirm.component.ts
@@ -1,29 +1,13 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { StorehousesService } from 'src/app/services/storehouses/storehouses.service';
 import { ErrorMessagesService } from 'src/app/utils/error-messages/error-messages.service';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { SuccessMessagesService } from 'src/app/utils/success-messages/success-messages.service';
 
 @Component({
   selector: 'ngbd-modal-confirm',
-  template: `
-  <div class="modal-header">
-    <h4 class="modal-title" id="modal-title">{{ data.titleModal }}</h4>
-    <button type="button" class="close" aria-describedby="modal-title" (click)="modal.dismiss('Cross click')">
-      <span aria-hidden="true">&times;</span>
-    </button>
-  </div>
-  <div class="modal-body">
-    <p><strong>{{ data.msgQuestionDeleteOne }}</strong></p>
-    <p>{{ data.msgQuestionDeleteTwo }}
-    <span class="text-danger">Essa operação poderá não ser desfeita.</span>
-    </p>
-  </div>
-  <div class="modal-footer">
-    <button type="button" class="btn btn-outline-secondary" (click)="modal.dismiss('cancel click')">Cancel</button>
-    <button type="button" class="btn btn-danger" (click)="modal.close(deleteBack())">Ok</button>
-  </div>
-  `
+  templateUrl: './ngbd-modal-confirm.html',
+styleUrls: ['./ngbd-modal-confirm.scss'],
 })
 
 
@@ -37,11 +21,18 @@ export class NgbdModalConfirmComponent {
   @Output() delete: EventEmitter<any> = new EventEmitter();
 
   constructor(
-    public modal: NgbActiveModal
+    public modal: NgbActiveModal,
+    config: NgbModalConfig,
+    private modalService: NgbModal
   ) {
+    config.backdrop = 'static';
+    config.keyboard = false;
   }
 
   deleteBack() {
     this.delete.emit(this.item);
+  }
+  open(content) {
+    this.modalService.open(content, { size: 'lg' });
   }
 }
