dca7c42db220bfc3ab5423c1e4739e291508ad66
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2021-11-30 16:26:53 -0300

    correção bug v1
    correção bug v1

--- src/app/layout/company-services/edit/edit.component.ts
+++ src/app/layout/company-services/edit/edit.component.ts
@@ -1,3 +1,4 @@
+import { MenuService } from 'src/app/services/menu-services/menu-services.service';
 import { CurrencyPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
@@ -15,188 +16,220 @@ import { Masks } from 'src/app/utils/masks';
 import { CaseInsensitive } from 'src/app/utils/case-insensitive';
 
 @Component({
-  selector: 'app-edit',
-  templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss'],
-  animations: [routerTransition()]
+    selector: 'app-edit',
+    templateUrl: './edit.component.html',
+    styleUrls: ['./edit.component.scss'],
+    animations: [routerTransition()]
 })
 export class EditComponent implements OnInit {
-  companyservice: any;
-  serviceForm: FormGroup;
-  profilesList: any = [];
-  loading: Boolean = false;
-  companies: any = [];
-  documentsAll: any = [];
-  searchSubscribe = '';
-  services: any = [];
-  userExternal = false;
-  // public mask = [/[1-4]/, /\d/, /\d/, /\d/, ',', /\d/, /\d/]
-  permissionEdit: any;
-  permissionDelete: any;
-  id: any;
+    companyservice: any;
+    serviceForm: FormGroup;
+    profilesList: any = [];
+    loading: Boolean = false;
+    companies: any = [];
+    documentsAll: any = [];
+    searchSubscribe = '';
+    services: any = [];
+    userExternal = false;
+    // public mask = [/[1-4]/, /\d/, /\d/, /\d/, ',', /\d/, /\d/]
+    permissionEdit: any;
+    permissionDelete: any;
+    id: any;
+    menuServices: any = [];
+    isDisabled = false;
+    selectedService?: MenuService;
 
+    constructor(
+        private mask: Masks,
+        private route: ActivatedRoute,
+        private _route: Router,
+        private fb: FormBuilder,
+        private successMsgSrv: SuccessMessagesService,
+        private errorMsg: ErrorMessagesService,
+        private companiesSrv: CompaniesService,
+        private companyServiceSrv: CompanyServicesService,
+        private currencyPipe: CurrencyPipe,
+        private menuSrv: MenuService,
+        private utilCase: CaseInsensitive
+    ) {}
 
-  constructor(
-    private mask: Masks,
-    private route: ActivatedRoute,
-    private _route: Router,
-    private fb: FormBuilder,
-    private successMsgSrv: SuccessMessagesService,
-    private errorMsg: ErrorMessagesService,
-    private companiesSrv: CompaniesService,
-    private companyServiceSrv: CompanyServicesService,
-    private currencyPipe: CurrencyPipe,
-    private utilCase: CaseInsensitive
-  ) {
-  }
+    ngOnInit() {
+        this.serviceForm = this.fb.group({
+        _id: '',
+        company: this.fb.control({ value: '' }, [Validators.required]),
+        services: this.fb.array(this.services)
+        });
 
-  ngOnInit() {
-    this.serviceForm = this.fb.group({
-      _id: '',
-      company: this.fb.control({ value: '' }, [Validators.required]),
-      services: this.fb.array(this.services)
-    });
+        this.id = this.route.snapshot.paramMap.get('id');
+        this.getCompanies();
+        this.getCompanyService();
+        this.getMenuServices();
 
-    this.permissionEdit = JSON.parse(window.localStorage.getItem('actions'))[0].change;
-    this.permissionDelete = JSON.parse(window.localStorage.getItem('actions'))[0].delete;
+        this.permissionEdit = JSON.parse(window.localStorage.getItem('actions'))[0].change;
+        this.permissionDelete = JSON.parse(window.localStorage.getItem('actions'))[0].delete;
+    }
 
-    this.id = this.route.snapshot.paramMap.get('id');
-    this.getCompanies();
-    this.getCompanyService();
+    get company() {
+        return this.serviceForm.get('company');
+    }
 
-    // this.addService();
-  }
+    createService(): FormGroup {
+        return this.fb.group({
+            description: '',
+            price: ''
+        });
+    }
 
-  getCompanyService() {
-    this.companyServiceSrv.service(this.id).subscribe(data => {
-      data.services = this.getDescriptionService(data.services);
-      this.companyservice = {
-        _links: data._links,
-        _id: data._id,
-        company: data.company,
-        services: data.services
-      };
+    addService(): void {
+        this.services = this.serviceForm.get('services') as FormArray;
+        this.services.push(this.createService());
+    }
 
-      this.serviceForm.patchValue({
-        _id: data._id,
-        company: data.company,
-        services: data.services
-      });
+    removeService(e) {
+        this.services.removeAt(e);
+    }
 
-      console.log(data.services);
+    formatter = (x: { name: string }) => x.name;
 
-      this.companyservice.services.map(item => {
-        this.addServiceExist(item);
-      });
-    }, error => {
-      console.log('ERROR:', error);
-    });
-  }
+    getCompanies() {
+        this.companiesSrv.searchCompanies().subscribe(
+            data => {
+                this.companies = data.items;
+            },
+            error => {
+                this.errorMsg.errorMessages(error);
+                console.log('ERROR: ', error);
+            }
+        );
+    }
 
-  getDescriptionService(items) {
-    const newItems = [];
-    items.forEach((item, i) => {
-      newItems.push({
-        _id: item._id,
-        description: item.description.descriptionService,
-        price: item.price
-      });
-    });
-    return newItems;
-  }
+    searchCompany = (text$: Observable<string>) =>
+        text$.pipe(
+            debounceTime(200),
+            distinctUntilChanged(),
+            map(company => {
+                let res;
+                if (company.length < 2) { [];
+                } else {
+                    res = _.filter(this.companies,
+                        v => (this.utilCase.replaceSpecialChars(v.name).toLowerCase().indexOf(company.toLowerCase())) > -1).slice(0, 10); }
+                return res;
+            })
+        )
 
-  createServiceExist(item): FormGroup {
-    return this.fb.group({
-      _id: item._id,
-      description: {value: item.description, disabled: true},
-      price: String(item.price).replace('.',',')
-    });
-  }
+    returnFormatCompanyServices() {
+        this.serviceForm.value.company = this.serviceForm.value.company._id;
+        const newArray = [];
+        this.serviceForm.value.services.map((item) => {
+            let priceStr = item.price.replace(',', '.');
+            priceStr = priceStr.replace('R$', '');
+            const priceFloat = parseFloat(priceStr);
+            newArray.push({ description: item.description, price: priceFloat, _id: item._id });
+        });
+        this.serviceForm.value.services = newArray;
+    }
 
-  addServiceExist(item): void {
-    this.services = this.serviceForm.get('services') as FormArray;
-    this.services.push(this.createServiceExist(item));
-  }
+    formatterMenuService = (x: { descriptionService: string }) => x.descriptionService;
 
-  get company() {
-    return this.serviceForm.get('company');
-  }
+    getMenuServices () {
+        this.menuSrv.listSelect().subscribe(data => {
+            this.menuServices = data.items;
+        }, error => {
+            console.log('ERROR: ', error);
+        });
+    }
 
-  createService(): FormGroup {
-    return this.fb.group({
-      description: '',
-      price: ''
-    });
-  }
+    searchMenu = (text$: Observable<string>) =>
+        text$.pipe(
+            debounceTime(200),
+            distinctUntilChanged(),
+        map(menu => {
+            let res;
+            if (menu.length < 2) {
+                [];
+            } else {
+                res = _.filter(this.menuServices, v => v.descriptionService.toLowerCase().indexOf(menu.toLowerCase()) > -1).slice(0, 10);
+                console.log('asdasdasdasdasda', res);
+            }
+            return res;
+        })
+        )
 
-  addService(): void {
-    this.services = this.serviceForm.get('services') as FormArray;
-    this.services.push(this.createService());
-  }
+    getCompanyService() {
+        this.companyServiceSrv.service(this.id).subscribe(data => {
+            data.services = this.getDescriptionService(data.services);
+            this.companyservice = {
+                _links: data._links,
+                _id: data._id,
+                company: data.company,
+                services: data.services
+            };
+            this.serviceForm.patchValue({
+                _id: data._id,
+                company: data.company,
+                services: data.services
+            });
+            console.log(data.services);
+            this.companyservice.services.map(item => {
+                this.addServiceExist(item);
+            });
+        },
+        error => {
+            console.log('ERROR:', error);
+        });
+    }
 
-  removeService(e) {
-    this.services.removeAt(e);
-  }
+    getDescriptionService(items) {
+        const newItems = [];
+        items.forEach((item, i) => {
+            newItems.push({
+                _id: item._id,
+                description: item.description.descriptionService,
+                price: item.price
+            });
+        });
+        return newItems;
+    }
 
+    createServiceExist(item): FormGroup {
+        this.isDisabled = true;
+        return this.fb.group({
+            _id: item._id,
+            description: {value: item.description, disabled: this.isDisabled},
+            price: String(item.price).replace('.', ',')
+        });
+    }
 
+    addServiceExist(item): void {
+        this.services = this.serviceForm.get('services') as FormArray;
+        this.services.push(this.createServiceExist(item));
+    }
 
-  formatter = (x: { name: string }) => x.name;
+    updateCompanyService() {
+        this.loading = true;
+        this.returnFormatCompanyServices();
 
-  getCompanies() {
-    this.companiesSrv.searchCompanies().subscribe(
-      data => {
-        this.companies = data.items;
-      },
-      error => {
-        this.errorMsg.errorMessages(error);
-        console.log('ERROR: ', error);
-      }
-    );
-  }
+        this.companyServiceSrv.updateService(this.serviceForm.value).subscribe(
+            data => {
+                if (data._id) {
+                    this.loading = false;
+                    this.successMsgSrv.successMessages('Serviço Empresarial alterado com sucesso.');
+                    this._route.navigate(['/company-services/get', data._id]);
+                }
+            },
+            error => {
+                this.loading = false;
+                this.errorMsg.errorMessages(error);
+                console.log('ERROR: ', error);
+            }
+        );
+    }
 
-  searchCompany = (text$: Observable<string>) =>
-    text$.pipe(
-      debounceTime(200),
-      distinctUntilChanged(),
-      map(company => {
-        let res;
-        if (company.length < 2) { []; } else { res = _.filter(this.companies, v => (this.utilCase.replaceSpecialChars(v.name).toLowerCase().indexOf(company.toLowerCase())) > -1).slice(0, 10); }
-        return res;
-      })
-    )
+    toBack() {
+        this._route.navigate([`/${'company-services/get'}`, this.id]);
+    }
 
-  returnFormatCompanyServices() {
-    this.serviceForm.value.company = this.serviceForm.value.company._id;
-    const newArray = [];
-    this.serviceForm.value.services.map((item) => {
-      let priceStr = item.price.replace(',', '.');
-      priceStr = priceStr.replace('R$', '');
-      const priceFloat = parseFloat(priceStr);
-      newArray.push({ description: item.description, price: priceFloat, _id: item._id });
-    });
-    this.serviceForm.value.services = newArray;
-  }
-
-  updateCompanyService() {
-    this.loading = true;
-    this.returnFormatCompanyServices();
-
-    this.companyServiceSrv.updateService(this.serviceForm.value).subscribe(
-      data => {
-        if (data._id) {
-          this.loading = false;
-          this.successMsgSrv.successMessages('Serviço Empresarial alterado com sucesso.');
-          this._route.navigate(['/company-services/get', data._id]);
-        }
-      },
-      error => {
-        this.loading = false;
-        this.errorMsg.errorMessages(error);
-        console.log('ERROR: ', error);
-      }
-    );
-  }
-  toBack() {
-    this._route.navigate([`/${'company-services/get'}`, this.id]);
-}
+    onSelect(menuService: MenuService): void {
+        this.selectedService = menuService;
+    }
 }
