c38099e920e3bd2f4e7b89478f1659e4059cb349
Wellington Carvalho <wellingtondevops@gmail.com>
2021-12-03 17:03:07 -0300

    merge com correcoes de bug na importacao de arquivos volumes e alterar servicos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,30 @@
+// export const environment = {
+
+//   apiUrl: 'https://archiomain.archio.com.br',
+//   apiUrlUpload: 'https://muploadprod.archio.com.br',
+//   apiUrlUploadSingle: 'https://smuploadprod.archio.com.br',
+//   exportUrl: 'https://exportarchivesprod.archio.com.br',
+//   urlSearch: 'https://archiomainprod.archio.com.br',
+//   production: true,
+//   firebaseConfig : {
+//     apiKey: 'AIzaSyCy9X4r36l6o1PLn4DuVdFpO41P5SmA57g',
+//     authDomain: 'archionotifier.firebaseapp.com',
+//     databaseURL: 'https://archionotifier-default-rtdb.firebaseio.com',
+//     projectId: 'archionotifier',
+//     storageBucket: 'archionotifier.appspot.com',
+//     messagingSenderId: '805529833577',
+//     appId: '1:805529833577:web:9e5bfaa56d5a4b2ff4d2f8',
+//     measurementId: 'G-DWEG5M9FGZ'
+//   }
+// };
 export const environment = {
 
-  apiUrl: 'https://archiomain.archio.com.br',
-  apiUrlUpload: 'https://muploadprod.archio.com.br',
-  apiUrlUploadSingle: 'https://smuploadprod.archio.com.br',
-  exportUrl: 'https://exportarchivesprod.archio.com.br',
-  urlSearch: 'https://archiomainprod.archio.com.br',
+  apiUrl: 'https://archiomaintestes.archio.com.br',
+  // apiUrl: 'http://localhost:3000',
+  apiUrlUpload: 'https://muploadtestes.archio.com.br',
+  apiUrlUploadSingle: 'https://smuploadtestes.archio.com.br',
+  exportUrl: 'https://exportarchivestestes.archio.com.br',
+  urlSearch: 'https://archiomaintestes.archio.com.br',
   production: true,
   firebaseConfig : {
     apiKey: 'AIzaSyCy9X4r36l6o1PLn4DuVdFpO41P5SmA57g',
