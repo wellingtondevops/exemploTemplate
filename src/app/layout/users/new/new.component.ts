d6be9ecd68d627357982f11db831e03bfbdaceff
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2021-12-15 15:51:18 -0300

    newUser Modal
    newUser Modal

--- src/app/layout/users/new/new.component.ts
+++ src/app/layout/users/new/new.component.ts
@@ -1,3 +1,4 @@
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { routerTransition } from '../../../router.animations';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
@@ -37,6 +38,7 @@ export class NewComponent implements OnInit {
     private _route: Router,
     private userSrv: UsersService,
     private fb: FormBuilder,
+    private modalService: NgbModal,
     private successMsgSrv: SuccessMessagesService,
     private errorMsg: ErrorMessagesService,
     private companiesSrv: CompaniesService,
@@ -199,6 +201,10 @@ export class NewComponent implements OnInit {
         console.log('ERROR: ', error);
       }
     );
+  }
+
+  open(content) {
+    this.modalService.open(content, {size: 'lg', windowClass: 'my-class'});
   }
 }
 
