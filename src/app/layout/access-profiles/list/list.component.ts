d90aa8a5d56e9d252a9d0fd28b5f6690446b1501
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2022-01-26 15:10:15 -0300

    createList
    create List

--- src/app/layout/access-profiles/list/list.component.ts
+++ src/app/layout/access-profiles/list/list.component.ts
@@ -1,52 +1,26 @@
-import { SaveLocal } from './../../../storage/saveLocal';
-import { Component, OnInit } from '@angular/core';
-import { routerTransition } from '../../../router.animations';
-import { UsersService } from '../../../services/users/users.service';
-import { UserList } from '../../../models/user';
-import { Router } from '@angular/router';
-import { ErrorMessagesService } from 'src/app/utils/error-messages/error-messages.service';
-import { Pipes } from 'src/app/utils/pipes/pipes';
-import { NgbdModalConfirmComponent } from '../../../shared/modules/ngbd-modal-confirm/ngbd-modal-confirm.component';
+import { SaveLocal } from 'src/app/storage/saveLocal';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Pipes } from 'src/app/utils/pipes/pipes';
+import { ErrorMessagesService } from 'src/app/utils/error-messages/error-messages.service';
 import { SuccessMessagesService } from 'src/app/utils/success-messages/success-messages.service';
-import { Page } from 'src/app/models/page';
+import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import _ from 'lodash';
+import { Component, OnInit } from '@angular/core';
+import { routerTransition } from 'src/app/router.animations';
 
-const MODALS = {
-    focusFirst: NgbdModalConfirmComponent
-};
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss'],
-  animations: [routerTransition()]
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.scss'],
+    animations: [routerTransition()]
 })
 export class ListComponent implements OnInit {
-    public isCollapsed = false;
-    searchForm: FormGroup;
-    users: UserList = {
-        _links: {
-            currentPage: 0,
-            foundItems: 0,
-            next: '',
-            self: '',
-            totalPage: 0
-        },
-        items: []
-    };
-    page = new Page();
-    loading: Boolean = true;
+    loading: Boolean = false;
+    permissionNew: Boolean = true;
+    profileForm: FormGroup;
 
-    columns = [
-        { name: 'Nome', prop: 'name' },
-        { name: 'E-mail', prop: 'email' },
-        { name: 'Criado em', prop: 'dateCreated', pipe: { transform: this.pipes.datePipe } }
-    ];
-  permissionNew: boolean = false;
 
     constructor(
-        private usersSrv: UsersService,
         private _route: Router,
         private successMsgSrv: SuccessMessagesService,
         private errorMsg: ErrorMessagesService,
@@ -55,100 +29,22 @@ export class ListComponent implements OnInit {
         public modal: NgbActiveModal,
         private fb: FormBuilder,
         private localStorageSrv: SaveLocal,
-    ) {}
+    ) { }
 
     ngOnInit() {
-        // this.usersList();
-        this.setPage({ offset: 0 });
-        this.searchForm = this.fb.group({
-            name: this.fb.control(null, Validators.required),
-            email: this.fb.control(null),
+        this.profileForm = this.fb.group({
+            company: this.fb.control(null, Validators.required),
+            name: this.fb.control(null)
         });
-        const user = JSON.parse(this.localStorageSrv.get('user'));
-        if (user && user.name) {
-            this.searchForm.patchValue({
-            name: user.name
-        });
-        }
-        this.permissionNew = JSON.parse(window.localStorage.getItem('actions'))[0].write
+        this.permissionNew = JSON.parse(window.localStorage.getItem('actions'))[0].write;
     }
 
-    get name() {
-        return this.searchForm.get('name');
-    }
-    get email() {
-        return this.searchForm.get('email');
+    get company() {
+        return this.profileForm.get('company');
     }
 
-    /* usersList() {
-        this.usersSrv.users(null).subscribe(
-            data => {
-                this.loading = false;
-                this.users = data;
-                this.page.pageNumber = data._links.currentPage;
-                this.page.totalElements = data._links.foundItems;
-                this.page.size = data._links.totalPage;
-            },
-            error => {
-                this.loading = false;
-                this.errorMsg.errorMessages(error);
-                console.log('ERROR: ', error);
-            }
-        );
-    } */
-
-    getUser(user) {
-        this._route.navigate(['/users/get', user._id]);
+    get profile() {
+        return this.profileForm.get('name');
     }
 
-    getUsers() {
-        this.setPageUsers({ offset: 0 });
-    }
-
-    setPageUsers(pageInfo) {
-        this.loading = true;
-        this.page.pageNumber = pageInfo.offset;
-        this.localStorageSrv.save('user', this.searchForm.value);
-        this.usersSrv.searchUsers(this.searchForm.value, this.page).subscribe(
-            data => {
-                this.users = data;
-                this.page.pageNumber = data._links.currentPage - 1;
-                this.page.totalElements = data._links.foundItems;
-                this.page.size = data._links.totalPage;
-                this.loading = false;
-            },
-            error => {
-                this.errorMsg.errorMessages(error);
-                console.log('ERROR: ', error);
-                this.loading = false;
-            }
-        );
-    }
-
-    setPage(pageInfo) {
-        this.loading = true;
-        this.page.pageNumber = pageInfo.offset;
-        this.usersSrv.users(this.page).subscribe(
-            data => {
-                this.users = data;
-                this.page.pageNumber = data._links.currentPage - 1;
-                this.page.totalElements = data._links.foundItems;
-                this.page.size = data._links.totalPage;
-                this.loading = false;
-            },
-            error => {
-                this.errorMsg.errorMessages(error);
-                console.log('ERROR: ', error);
-                this.loading = false;
-            }
-        );
-    }
-    clear() {
-        this.localStorageSrv.clear('user');
-
-        this.searchForm.patchValue({
-            name: null,
-            email: null
-        });
-    }
 }
