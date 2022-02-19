eb741ab8f3bcb87c195e6726d098b6abd25efff2
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2022-02-19 10:32:51 -0300

    newInputDocuments
    new Input Documents

--- src/app/layout/documents/list/list.component.ts
+++ src/app/layout/documents/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { routerTransition } from '../../../router.animations';
 import { DocumentsService } from 'src/app/services/documents/documents.service';
 import { DocumentList } from 'src/app/models/document';
@@ -8,172 +9,182 @@ import { Page } from 'src/app/models/page';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CompaniesService } from 'src/app/services/companies/companies.service';
-import { Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { Observable, Subject, merge } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, filter } from 'rxjs/operators';
 import _ from 'lodash';
 import { SaveLocal } from '../../../storage/saveLocal';
 import { CaseInsensitive } from 'src/app/utils/case-insensitive';
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss'],
-  animations: [routerTransition()]
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.scss'],
+    animations: [routerTransition()]
 })
 export class ListComponent implements OnInit {
-  searchForm: FormGroup;
-  companies: any = [];
-  documents: DocumentList = {
-    _links: {
-      currentPage: 0,
-      foundItems: 0,
-      next: '',
-      self: '',
-      totalPage: 0
-    },
-    items: []
-  };
-  page = new Page();
-  columns = [
-    { name: 'Nome', prop: 'name' },
-    { name: 'Fase Corrente', prop: 'dcurrentValue'},
-    { name: 'Fase Intermédiaria', prop: 'dintermediateValue'},
-    { name: 'Destinação Final', prop: 'dfinal'},
-
-  ];
-  loading: Boolean = false;
-  permissionNew = false;
-
-  constructor(
-    private _route: Router,
-    private documentSrv: DocumentsService,
-    private errorMsg: ErrorMessagesService,
-    private companiesSrv: CompaniesService,
-    private pipes: Pipes,
-    private fb: FormBuilder,
-    private localStorageSrv: SaveLocal,
-    private utilCase: CaseInsensitive
-  ) { }
-
-  ngOnInit() {
-
-
-    this.searchForm = this.fb.group({
-      name: this.fb.control(null),
-      company: this.fb.control(null, [Validators.required]),
-    });
-    const document = JSON.parse(this.localStorageSrv.get('document'));
-    if (document && document.company) {
-      this.searchForm.patchValue({
-        company: document.company,
-        name: document.name
-      });
-    }
-    this.getCompanies();
-    this.getDocuments();
-    this.permissionNew = JSON.parse(window.localStorage.getItem('actions'))[0].write;
-  }
-
-  get company() {
-    return this.searchForm.get('company');
-  }
-
-  getCompanies() {
-    this.companiesSrv.searchCompanies().subscribe(
-      data => {
-        this.companies = data.items;
-      },
-      error => {
-        this.errorMsg.errorMessages(error);
-        console.log('ERROR: ', error);
-        this.loading = false;
-      }
-    );
-  }
-
-  getDocuments() {
-    this.setPageDocuments({ offset: 0 });
-  }
-
-  returnId(object) {
-    this.searchForm.value[object] = _.filter(this.searchForm.value[object], function (value, key) {
-      if (key === '_id') { return value; }
-    })[0];
-  }
-
-  setPageDocuments(pageInfo) {
-    this.loading = true;
-    this.page.pageNumber = pageInfo.offset;
-    this.localStorageSrv.save('documents', this.searchForm.value);
-
-    const newForm = {
-        company: null,
-        name: null,
+    @ViewChild('instanceCompany',) instanceCompany: NgbTypeahead;
+    searchForm: FormGroup;
+    companies: any = [];
+    focusCompany$ = new Subject<string>();
+    clickCompany$ = new Subject<string>();
+    documents: DocumentList = {
+        _links: {
+            currentPage: 0,
+            foundItems: 0,
+            next: '',
+            self: '',
+            totalPage: 0
+        },
+        items: []
     };
+    page = new Page();
+    columns = [
+        { name: 'Nome', prop: 'name' },
+        { name: 'Fase Corrente', prop: 'dcurrentValue' },
+        { name: 'Fase Intermédiaria', prop: 'dintermediateValue' },
+        { name: 'Destinação Final', prop: 'dfinal' },
 
-    this.searchForm.value.company ? newForm.company = this.searchForm.value.company : null;
-    this.searchForm.value.name ? newForm.name = this.searchForm.value.name : null;
+    ];
+    loading: Boolean = false;
+    permissionNew = false;
 
-    const searchValue = _.omitBy(newForm, _.isNil);
+    constructor(
+        private _route: Router,
+        private documentSrv: DocumentsService,
+        private errorMsg: ErrorMessagesService,
+        private companiesSrv: CompaniesService,
+        private pipes: Pipes,
+        private fb: FormBuilder,
+        private localStorageSrv: SaveLocal,
+        private utilCase: CaseInsensitive
+    ) { }
 
-    this.documentSrv.searchDocts(searchValue, this.page).subscribe(
-        data => {
-      this.page.pageNumber = data._links.currentPage;
-      this.page.totalElements = data._links.foundItems;
-      this.page.size = data._links.totalPage;
-      this.documents = data;
-      this.loading = false;
-    }, error => {
-      console.log('ERROR: ', error);
-      this.loading = false;
-    });
-  }
+    ngOnInit() {
 
-//   setPage(pageInfo) {
-//     this.loading = true;
-//     this.page.pageNumber = pageInfo.offset;
 
-//     this.documentSrv.documents(this.page).subscribe(
-//       data => {
-//         console.log(data);
-//         this.documents = data;
-//         this.page.pageNumber = data._links.currentPage - 1;
-//         this.page.totalElements = data._links.foundItems;
-//         this.page.size = data._links.totalPage;
-//         this.loading = false;
-//       },
-//       error => {
-//         this.errorMsg.errorMessages(error);
-//         console.log('ERROR: ', error);
-//         this.loading = false;
-//       }
-//     );
-//   }
+        this.searchForm = this.fb.group({
+            name: this.fb.control(null),
+            company: this.fb.control(null, [Validators.required]),
+        });
+        const document = JSON.parse(this.localStorageSrv.get('document'));
+        if (document && document.company) {
+            this.searchForm.patchValue({
+                company: document.company,
+                name: document.name
+            });
+        }
+        this.getCompanies();
+        this.getDocuments();
+        this.permissionNew = JSON.parse(window.localStorage.getItem('actions'))[0].write;
+    }
 
-  getDocument(document) {
-    this._route.navigate(['/documents/get', document._id]);
-  }
+    get company() {
+        return this.searchForm.get('company');
+    }
 
-  formatter = (x: { name: string }) => x.name;
+    getCompanies() {
+        this.companiesSrv.searchCompanies().subscribe(
+            data => {
+                this.companies = data.items;
+            },
+            error => {
+                this.errorMsg.errorMessages(error);
+                console.log('ERROR: ', error);
+                this.loading = false;
+            }
+        );
+    }
 
-  searchCompany = (text$: Observable<string>) =>
-    text$.pipe(
-      debounceTime(200),
-      distinctUntilChanged(),
-      map(company => {
-        const res = [];
-        if (company.length < 2) { []; }
-        return _.filter(this.companies, v => (this.utilCase.replaceSpecialChars(v.name).toLowerCase().indexOf(company.toLowerCase())) > -1).slice(0, 10);
+    getDocuments() {
+        this.setPageDocuments({ offset: 0 });
+    }
 
-      })
-    )
+    returnId(object) {
+        this.searchForm.value[object] = _.filter(this.searchForm.value[object], function (value, key) {
+            if (key === '_id') { return value; }
+        })[0];
+    }
 
-  clear() {
-    this.localStorageSrv.clear('document');
+    setPageDocuments(pageInfo) {
+        this.loading = true;
+        this.page.pageNumber = pageInfo.offset;
+        this.localStorageSrv.save('documents', this.searchForm.value);
 
-    this.searchForm.patchValue({
-      company: null,
-      name: null
-    });
-  }
+        const newForm = {
+            company: null,
+            name: null,
+        };
+
+        this.searchForm.value.company ? newForm.company = this.searchForm.value.company : null;
+        this.searchForm.value.name ? newForm.name = this.searchForm.value.name : null;
+
+        const searchValue = _.omitBy(newForm, _.isNil);
+
+        this.documentSrv.searchDocts(searchValue, this.page).subscribe(
+            data => {
+                this.page.pageNumber = data._links.currentPage;
+                this.page.totalElements = data._links.foundItems;
+                this.page.size = data._links.totalPage;
+                this.documents = data;
+                this.loading = false;
+            }, error => {
+                console.log('ERROR: ', error);
+                this.loading = false;
+            });
+    }
+
+    //   setPage(pageInfo) {
+    //     this.loading = true;
+    //     this.page.pageNumber = pageInfo.offset;
+
+    //     this.documentSrv.documents(this.page).subscribe(
+    //       data => {
+    //         console.log(data);
+    //         this.documents = data;
+    //         this.page.pageNumber = data._links.currentPage - 1;
+    //         this.page.totalElements = data._links.foundItems;
+    //         this.page.size = data._links.totalPage;
+    //         this.loading = false;
+    //       },
+    //       error => {
+    //         this.errorMsg.errorMessages(error);
+    //         console.log('ERROR: ', error);
+    //         this.loading = false;
+    //       }
+    //     );
+    //   }
+
+    getDocument(document) {
+        this._route.navigate(['/documents/get', document._id]);
+    }
+
+    formatter = (x: { name: string }) => x.name;
+
+    searchCompany = (text$: Observable<string>) => {
+        const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+        const clicksWithClosedPopup$ = this.clickCompany$.pipe(filter(() => !this.instanceCompany.isPopupOpen()));
+        const inputFocus$ = this.focusCompany$;
+
+        return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+            map(company => {
+                let res = [];
+                if (company.length < 0) {
+                    [];
+                } else {
+                    res = _.filter(this.companies,
+                        v => (this.utilCase.replaceSpecialChars(v.name).toLowerCase().indexOf(company.toLowerCase())) > -1).slice(0, 10);
+                }
+                return res;
+            })
+        );
+    }
+
+    clear() {
+        this.localStorageSrv.clear('document');
+
+        this.searchForm.patchValue({
+            company: null,
+            name: null
+        });
+    }
 }
