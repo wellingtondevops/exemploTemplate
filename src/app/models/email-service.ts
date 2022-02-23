2c635296e9cd442d6ed9cd01a7c55895d66ef4ea
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2022-02-23 10:48:51 -0300

    service&&modelEmail
    service && model Email

---
+++ src/app/models/email-service.ts
@@ -0,0 +1,20 @@
+import { Pagination } from './pagination';
+
+export class EmailService {
+    _links: {
+        self: string;
+    };
+    _id: string;
+    title: string;
+    archive: {
+        _id: string;
+        pending: boolean;
+    };
+    userSernder: {
+        _id: string,
+        name: string
+    };
+    highlighted: boolean;
+    dateCreated: string;
+    notes: string;
+}
