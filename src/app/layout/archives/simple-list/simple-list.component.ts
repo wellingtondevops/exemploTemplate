7d4aae30661fc024644bdc545b0b50fc6cf4e323
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2022-03-28 17:30:34 -0300

    fixNpmRun
    fixNpmRun

--- src/app/layout/archives/simple-list/simple-list.component.ts
+++ src/app/layout/archives/simple-list/simple-list.component.ts
@@ -408,13 +408,4 @@ export class SimpleListComponent implements OnInit {
     }
 }
 
-@Pipe({
-    name: 'enumToArray'
-})
-export class EnumToArrayPipe implements PipeTransform {
-    transform(data: Object) {
-        const keys = Object.keys(data);
-        return keys.slice(keys.length / 2);
-    }
-}
 
