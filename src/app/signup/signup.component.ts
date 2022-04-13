73045b7195f2c8a5dc1f3021d5cae3544b3a9111
Lucas A. Lisboa <lucasalisboa.ll@gmail.com>
2022-04-13 12:20:19 -0300

    newSignup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,3 +1,7 @@
+import { ErrorMessagesService } from 'src/app/utils/error-messages/error-messages.service';
+import { AuthService } from './../services/auth/auth.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../router.animations';
 import { TranslateService } from '@ngx-translate/core';
@@ -9,6 +13,28 @@ import { TranslateService } from '@ngx-translate/core';
     animations: [routerTransition()],
     providers: [TranslateService]
 })
-export class SignupComponent {
-    constructor() {}
+export class SignupComponent implements OnInit {
+    loading = false;
+    signupForm: FormGroup;
+
+    constructor(
+        public router: Router,
+        private fb: FormBuilder,
+        private AuthSrv: AuthService,
+        private errorMsg: ErrorMessagesService
+    ) {}
+
+    ngOnInit() {
+        this.loading = false;
+        this.signupForm = this.fb.group({
+            name: this.fb.control('', [Validators.required, Validators.minLength(3)]),
+            email: this.fb.control('', [Validators.required, Validators.email]),
+            password: this.fb.control('', [Validators.required, Validators.minLength(6)])
+        });
+
+    }
+
+    signupNow(){
+
+    }
 }
--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -1,15 +1,21 @@
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup.component';
+import { NgxLoadingModule } from 'ngx-loading';
+
 
 @NgModule({
   imports: [
     CommonModule,
     TranslateModule,
-    SignupRoutingModule
+    SignupRoutingModule,
+    NgxLoadingModule.forRoot({}),
+    ReactiveFormsModule,
+    FormsModule,
   ],
   declarations: [SignupComponent]
 })
