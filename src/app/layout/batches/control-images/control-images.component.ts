f286e70f189beb5421987b053fd120eb4912b17a
Lucas A. Lisboa <lucasalisboa.ll@gmail.com>
2022-04-14 17:34:24 -0300

    version 1
    version 1

--- src/app/layout/batches/control-images/control-images.component.ts
+++ src/app/layout/batches/control-images/control-images.component.ts
@@ -1,3 +1,5 @@
+import { SaveLocal } from 'src/app/storage/saveLocal';
+import { DocumentsService } from 'src/app/services/documents/documents.service';
 import { routerTransition } from 'src/app/router.animations';
 import { Page } from 'src/app/models/page';
 import { SuccessMessagesService } from 'src/app/utils/success-messages/success-messages.service';
@@ -6,6 +8,7 @@ import { Batch } from 'src/app/models/batch';
 import { BatchesService } from 'src/app/services/batches/batches.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
     selector: 'app-control-images',
@@ -20,6 +23,12 @@ export class ControlImagesComponent implements OnInit {
     page = new Page();
     batchImages: any;
     pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+    document: Document;
+    image: any;
+    urlFile: any = '';
+    pictureId: any = '';
+    pictures: any = '';
+    isPdf = false;
 
 
     constructor(
@@ -28,24 +37,39 @@ export class ControlImagesComponent implements OnInit {
         private batchesSrv: BatchesService,
         private successMsgSrv: SuccessMessagesService,
         private errorMsg: ErrorMessagesService,
+        private documentSrv: DocumentsService,
+        private localStorageSrv: SaveLocal,
+
+
     ) { }
 
     ngOnInit() {
-
+        $(function () {
+            $("#selectable").selectable();
+        });
         this.id = this.route.snapshot.paramMap.get('id');
         this.getBatch();
+
     }
 
     getBatch() {
-        this.loading = true;
-        this.batchesSrv.batch(this.id).subscribe(data => {
-            this.loading = false;
-            this.batch = data;
-            this.getBatchImages(1, 24);
+        this.page.pageNumber = 1;
+        this.batchesSrv.imagens(this.id, this.page, 1).subscribe(data => {
+            this.image = data.items[0];
+            if (data.items.length >= 1) {
+                this.urlFile = data.items[0].url;
+                this.pictureId = data.items[0]._id;
+                this.pictures = this.pictureId;
+                this.urlFile.indexOf('.pdf') !== -1 ? this.isPdf = true : '';
+                this.getBatchImages();
+            } else {
+                this.getBatchImages();
+                setTimeout(function () { $('#open')[0].click(); }, 700);
+
+
+            }
         }, error => {
             console.log('ERROR: ', error);
-            this.loading = false;
-            this.errorMsg.errorMessages(error);
         });
     }
 
