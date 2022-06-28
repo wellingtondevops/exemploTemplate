a482fe0cb7d018e2223d8abed0ff79fc0d9d2d7f
Wellington Carvalho <wellingtondevops@gmail.com>
2022-06-28 09:46:46 -0300

    enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
 
   apiUrl: 'https://archiomaintestes.archio.com.br',
 
-  apiUrlUpload: 'https://muploadtestes.archio.com.br',
+  apiUrlUpload: 'http://localhost:3000',
   apiUrlUploadSingle: 'https://smuploadtestes.archio.com.br',
 //   exportUrl: 'https://exportarchivestestes.archio.com.br',
   urlSearch: 'https://archiomaintestes.archio.com.br',
