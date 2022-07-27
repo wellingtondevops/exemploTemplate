defebb71650d735aa2360a6a670ac831cd0c86b2
Wellington Carvalho <wellingtondevops@gmail.com>
2022-07-27 15:32:52 -0300

    enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,44 +1,23 @@
-// export const environment = {
-
-//     apiUrl: 'https://archiomaintestes.archio.com.br',
-//   exampleImportVolumesSimple: 'https://storage.googleapis.com/archiobucket/ARCHIOFILES/Exemplo%20Guarda%20Simples.xlsx',
-//   exampleImportVolumesManaged: 'https://storage.googleapis.com/archiobucket/ARCHIOFILES/Exemplo%20Guarda%20Gerenciada.xlsx',
-//   apiUrlUpload: 'https://archiomaintestes.archio.com.br',
-//   apiUrlUploadSingle: 'https://smuploadtestes.archio.com.br',
-// //   exportUrl: 'https://exportarchivestestes.archio.com.br',
-//   urlSearch: 'https://archiomaintestes.archio.com.br',
-//   production: false,
-//   firebaseConfig : {
-//     apiKey: 'AIzaSyCy9X4r36l6o1PLn4DuVdFpO41P5SmA57g',
-//     authDomain: 'archionotifier.firebaseapp.com',
-//     databaseURL: 'https://archionotifier-default-rtdb.firebaseio.com',
-//     projectId: 'archionotifier',
-//     storageBucket: 'archionotifier.appspot.com',
-//     messagingSenderId: '805529833577',
-//     appId: '1:805529833577:web:9e5bfaa56d5a4b2ff4d2f8',
-//     measurementId: 'G-DWEG5M9FGZ'
-//   }
-// };
 export const environment = {
 
-  apiUrl: 'https://archiomainprod.archio.com.br',
-exampleImportVolumesSimple: 'https://storage.googleapis.com/archiobucket/ARCHIOFILES/Exemplo%20Guarda%20Simples.xlsx',
-exampleImportVolumesManaged: 'https://storage.googleapis.com/archiobucket/ARCHIOFILES/Exemplo%20Guarda%20Gerenciada.xlsx',
-apiUrlUpload: 'https://archiomainprod.archio.com.br',
-apiUrlUploadSingle: 'https://smuploadprod.archio.com.br',
+    apiUrl: 'https://archiomaintestes.archio.com.br',
+  exampleImportVolumesSimple: 'https://storage.googleapis.com/archiobucket/ARCHIOFILES/Exemplo%20Guarda%20Simples.xlsx',
+  exampleImportVolumesManaged: 'https://storage.googleapis.com/archiobucket/ARCHIOFILES/Exemplo%20Guarda%20Gerenciada.xlsx',
+  apiUrlUpload: 'https://archiomaintestes.archio.com.br',
+  apiUrlUploadSingle: 'https://smuploadtestes.archio.com.br',
 //   exportUrl: 'https://exportarchivestestes.archio.com.br',
-urlSearch: 'https://archiomainprod.archio.com.br',
-production: false,
-firebaseConfig : {
-  apiKey: 'AIzaSyCy9X4r36l6o1PLn4DuVdFpO41P5SmA57g',
-  authDomain: 'archionotifier.firebaseapp.com',
-  databaseURL: 'https://archionotifier-default-rtdb.firebaseio.com',
-  projectId: 'archionotifier',
-  storageBucket: 'archionotifier.appspot.com',
-  messagingSenderId: '805529833577',
-  appId: '1:805529833577:web:9e5bfaa56d5a4b2ff4d2f8',
-  measurementId: 'G-DWEG5M9FGZ'
-}
+  urlSearch: 'https://archiomaintestes.archio.com.br',
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyCy9X4r36l6o1PLn4DuVdFpO41P5SmA57g',
+    authDomain: 'archionotifier.firebaseapp.com',
+    databaseURL: 'https://archionotifier-default-rtdb.firebaseio.com',
+    projectId: 'archionotifier',
+    storageBucket: 'archionotifier.appspot.com',
+    messagingSenderId: '805529833577',
+    appId: '1:805529833577:web:9e5bfaa56d5a4b2ff4d2f8',
+    measurementId: 'G-DWEG5M9FGZ'
+  }
 };
 
 
