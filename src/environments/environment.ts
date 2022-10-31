222e9c64d2dde7beb2650c6bc7ae68da791ec27b
Wellington Carvalho <wellingtondevops@gmail.com>
2022-10-31 10:28:18 -0300

    ajustes no firebase menssageria

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment = {
     urlSearch: 'https://archiomaintestes.archio.com.br',
     production: false,
     firebaseConfig: {
-        apiKey: 'AIzaSyCy9X4r36l6o1PLn4DuVdFpO41P5SmA57g',
-        authDomain: 'archionotifier.firebaseapp.com',
-        databaseURL: 'https://archionotifier-default-rtdb.firebaseio.com',
-        projectId: 'archionotifier',
-        storageBucket: 'archionotifier.appspot.com',
-        messagingSenderId: '805529833577',
-        appId: '1:805529833577:web:9e5bfaa56d5a4b2ff4d2f8',
-        measurementId: 'G-DWEG5M9FGZ'
+        apiKey: 'AIzaSyA2ebuRJXPPBjK4ptmXwlAmO68wHTafxPE',
+        authDomain: 'archionotifications.firebaseapp.com',
+        databaseURL: 'https://archionotifications-default-rtdb.firebaseio.com',
+        projectId: 'archionotifications',
+        storageBucket: 'archionotifications.appspot.com',
+        messagingSenderId: '669284299770',
+        appId: '1:669284299770:web:e80900ef6ac39318de2a43',
+        measurementId: 'G-64Q75QJXNL'
     }
 };
 
