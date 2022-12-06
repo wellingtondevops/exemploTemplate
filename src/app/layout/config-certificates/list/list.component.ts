a4ce2175b9ed2ec0de4366d18fa9cb2c33620dee
Lucas A. Lisboa <lucasalisboa.ll@gmail.com>
2022-12-06 19:55:57 -0300

    list work

--- src/app/layout/config-certificates/list/list.component.ts
+++ src/app/layout/config-certificates/list/list.component.ts
@@ -6,15 +6,16 @@ import { Certificate, CertificateList } from './../../../models/certificate';
 import { ErrorMessagesService } from './../../../utils/error-messages/error-messages.service';
 import { SaveLocal } from './../../../storage/saveLocal';
 import { IntroJsService } from './../../../services/introJs/intro-js.service';
-import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalOptions, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ColumnMode } from '@swimlane/ngx-datatable';
+import { ModalUploadCertificateComponent } from '../modalUploadCertificate/modalUploadCertificate.component';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css'],
+  styleUrls: ['./list.component.scss'],
   animations: [routerTransition()]
 })
 export class ListComponent implements OnInit {
@@ -65,6 +66,7 @@ export class ListComponent implements OnInit {
     this.searchForm = this.fb.group({
       labelCertificate: this.fb.control(''),
     });
+    this.getCertificates();
   }
 
   getCertificates() {
@@ -77,7 +79,7 @@ export class ListComponent implements OnInit {
     this.localStorageSrv.save('labelPackage', this.searchForm.value);
     this.certificateSrv.searchCertificate(this.page).subscribe(
       data => {
-        this.certificatesData = data;
+        this.certificatesData = data.items;
         this.page.pageNumber = data._links.currentPage - 1;
         this.page.totalElements = data._links.foundItems;
         this.page.size = data._links.totalPage;
@@ -88,7 +90,6 @@ export class ListComponent implements OnInit {
         this.loading = false;
       }
     );
-
   }
 
   clear() {
@@ -97,5 +98,37 @@ export class ListComponent implements OnInit {
       labelPackage: null
     });
   }
+
+  newCertificate(event) {
+    if (event.type == 'click') {
+      this.modalRef = this.modalService.open(ModalUploadCertificateComponent, this.modalOptions);
+
+      if (event.row) {
+        this.data = event.row;
+        event.cellElement.blur();
+        this.modalRef.componentInstance.doc = this.data;
+      }
+
+      this.modalRef.result.then((result) => {
+        if (result != "Sair") {
+          this.getCertificates();
+        };
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+    }
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a bac~kdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
 
 }
